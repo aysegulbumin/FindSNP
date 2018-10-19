adfbf470619eeb404255015c8ff55127aa1b91f2
aysegulbumin <aysegul.bumin@ufl.edu>
2018-10-19 19:09:06 -0400

    main.cpp added.

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <sstream>
+#include <map>
+#include <algorithm>
+#include <set>
+#include <functional>
+#include <cmath>
+#include <iostream>
+#include <fstream>
+#include "MultiSNP.h"
+int main(int argc, char** argv) {
+    std::cout << "SNP Finder is running." << std::endl;
+   
+    MultiSNP(argc,argv);
+
+    return 0;
+}
